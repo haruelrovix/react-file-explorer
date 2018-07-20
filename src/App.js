f02c0edfe153863ff88840e52c667c27e3799252
Havit_R <havitchoirul.rovik@mitrais.com>
2018-07-20 09:02:26 +0700

    Initial commit for react-file-explorer

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component, Fragment } from 'react';
+import SplitPane from 'react-split-pane';
+import { toggleExpandedForAll } from 'react-sortable-tree';
+import Demo from './Demo';
+import Editor from './Editor';
+// import Title from './Title';
+
+import treeData from './defaultTreeData';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      treeData,
+      pathStatus: ''
+    };
+
+    this.onChangeTreeData = this.onChangeTreeData.bind(this);
+    this.getNewTreeData = this.getNewTreeData.bind(this);
+    this.editorRef = this.editorRef.bind(this);
+    this.expand = this.expand.bind(this);
+    this.getVisibleNodeInfo = this.getVisibleNodeInfo.bind(this);
+  }
+
+  componentDidMount() {
+    if (this._editor) {
+      this._editor.set(treeData);
+      this._editor.expandAll();
+    }
+  }
+
+  onChangeTreeData(treeData) {
+    if (this._editor) {
+      this._editor.set(treeData);
+      this.setState({
+        treeData,
+      });
+    }
+  }
+
+  getNewTreeData() {
+    if (this._editor) {
+      this.setState({
+        treeData: this._editor.get(),
+      });
+    }
+  }
+
+  editorRef(editor) {
+    this._editor = editor;
+  }
+
+  expand(expanded) {
+    const newTreeData = toggleExpandedForAll({
+      treeData: this.state.treeData,
+      expanded,
+    });
+
+    this._editor.set(newTreeData);
+
+    this.setState({
+      treeData: newTreeData,
+    });
+  }
+
+  getVisibleNodeInfo(node) {
+    this.setState({ pathStatus: node.path.join(' > ') })
+  }
+
+  render() {
+    const { treeData } = this.state;
+
+    return (
+      <Fragment>
+        {/* <Title /> */}
+        <div style={{ margin: '12px' }}>
+          Manage Folders/Files using ReactJS
+        </div>
+        <SplitPane split="vertical" defaultSize="50%">
+          <Demo
+            onChangeTreeData={this.onChangeTreeData}
+            treeData={treeData}
+            expand={this.expand}
+            getVisibleNodeInfo={this.getVisibleNodeInfo}
+          />
+          {/* <SplitPane split="horizontal" defaultSize="25%">
+            <div /> */}
+          <div style={{ padding: '0 12px 12px' }}>
+            {this.state.pathStatus}
+            <div style={{ visibility: 'hidden' }}>
+              <Editor onChange={this.getNewTreeData} editorRef={this.editorRef} />
+            </div>
+          </div>
+          {/* </SplitPane> */}
+        </SplitPane>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/Demo.js
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import SortableTree from 'react-sortable-tree';
+import FileExplorerTheme from 'react-sortable-tree-theme-file-explorer';
+import styles from './Demo.module.css';
+
+const maxDepth = 5;
+
+// const alertNodeInfo = ({ node, path, treeIndex }) => {
+//   const objectString = Object.keys(node)
+//     .map(k => (k === 'children' ? 'children: Array' : `${k}: '${node[k]}'`))
+//     .join(',\n   ');
+
+//   global.alert(
+//     'Info passed to the button generator:\n\n' +
+//       `node: {\n   ${objectString}\n},\n` +
+//       `path: [${path.join(', ')}],\n` +
+//       `treeIndex: ${treeIndex}`
+//   );
+// };
+
+// const recordCall = (name, args) => {
+//   // eslint-disable-next-line no-console
+//   console.log(`${name} called with arguments:`, args);
+// };
+
+export default class Demo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.expandAll = this.expandAll.bind(this);
+    this.collapseAll = this.collapseAll.bind(this);
+    this.selectNextMatch = this.selectNextMatch.bind(this);
+    this.selectPrevMatch = this.selectPrevMatch.bind(this);
+    this.getNodeKey = this.getNodeKey.bind(this);
+    this.getVisibleNodeInfo = this.getVisibleNodeInfo.bind(this);
+
+    this.state = {
+      searchString: '',
+      searchFocusIndex: 0,
+      searchFoundCount: null,
+      treeData: [],
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    return { treeData: nextProps.treeData };
+  }
+
+  expandAll() {
+    this.props.expand(true);
+  }
+
+  collapseAll() {
+    this.props.expand(false);
+  }
+
+  selectPrevMatch() {
+    const { searchFocusIndex, searchFoundCount } = this.state;
+
+    this.setState({
+      searchFocusIndex:
+        searchFocusIndex !== null
+          ? (searchFoundCount + searchFocusIndex - 1) % searchFoundCount
+          : searchFoundCount - 1,
+    });
+  }
+
+  selectNextMatch() {
+    const { searchFocusIndex, searchFoundCount } = this.state;
+
+    this.setState({
+      searchFocusIndex:
+        searchFocusIndex !== null
+          ? (searchFocusIndex + 1) % searchFoundCount
+          : 0,
+    });
+  }
+
+  getNodeKey({ node }) {
+    return node.title;
+  }
+
+  getVisibleNodeInfo(rowInfo) {
+    // console.log({rowInfo});
+    // const a = getVisibleNodeInfoAtIndex(this.state.treeData, 1, this.getNodeKey);
+  }
+
+  render() {
+    const {
+      treeData,
+      searchString,
+      searchFocusIndex,
+      // searchFoundCount,
+    } = this.state;
+
+    const { onChangeTreeData } = this.props;
+
+    return (
+      <div className={styles.demoWrapper}>
+        {/* <div className={styles.buttons}>
+          <div>
+            <button className="btn btn-primary" onClick={this.expandAll}>
+              Expand All
+            </button>
+            <button className="btn btn-warning" onClick={this.collapseAll}>
+              Collapse All
+            </button>
+          </div>
+          <form
+            style={{ display: 'inline-block' }}
+            onSubmit={event => {
+              event.preventDefault();
+            }}
+          >
+            <label htmlFor="find-box">
+              Search:&nbsp;
+              <input
+                id="find-box"
+                type="text"
+                value={searchString}
+                onChange={event =>
+                  this.setState({ searchString: event.target.value })
+                }
+              />
+            </label>
+
+            <button
+              className="btn btn-info"
+              type="button"
+              disabled={!searchFoundCount}
+              onClick={this.selectPrevMatch}
+            >
+              &lt;
+            </button>
+
+            <button
+              className="btn btn-info"
+              type="submit"
+              disabled={!searchFoundCount}
+              onClick={this.selectNextMatch}
+            >
+              &gt;
+            </button>
+
+            <span>
+              &nbsp;
+              {searchFoundCount > 0 ? searchFocusIndex + 1 : 0}
+              {' / '}
+              {searchFoundCount || 0}
+            </span>
+          </form>
+        </div> */}
+        <div className={styles.treeWrapper}>
+          <SortableTree
+            treeData={treeData}
+            onChange={onChangeTreeData}
+            onMoveNode={({ node, treeIndex, path }) =>
+              global.console.debug(
+                'node:',
+                node,
+                'treeIndex:',
+                treeIndex,
+                'path:',
+                path
+              )
+            }
+            // onVisibilityToggle={args => recordCall('onVisibilityToggle', args)}
+            maxDepth={maxDepth}
+            searchQuery={searchString}
+            searchFocusOffset={searchFocusIndex}
+            canDrag={({ node }) => !node.noDragging}
+            canDrop={({ nextParent }) => !nextParent || !nextParent.noChildren}
+            searchFinishCallback={matches =>
+              this.setState({
+                searchFoundCount: matches.length,
+                searchFocusIndex:
+                  matches.length > 0 ? searchFocusIndex % matches.length : 0,
+              })
+            }
+            getNodeKey={this.getNodeKey}
+            isVirtualized={true}
+            generateNodeProps={rowInfo => ({
+              buttons: [
+                <button
+                  className="btn btn-outline-success"
+                  style={{
+                    verticalAlign: 'middle',
+                  }}
+                  onClick={() => this.props.getVisibleNodeInfo(rowInfo)}
+                >
+                  ℹ
+                </button>,
+              ],
+            })}
+            theme={FileExplorerTheme}
+          />
+        </div>
+      </div>
+    );
+  }
+}
